9812958c6198b240657d6a3480e6491b6d67568c
Fajarihsan <7afajarihsan@gmail.com>
2026-04-15 14:55:51 +0000

    Edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
             args: "start -p 3005",
             cwd: "./",
             exec_mode: "cluster",
-            instances: "max",
+            instances: "2",
             autorestart: true,
             watch: false,
             max_memory_restart: "3G",
